6e4526af20b6fa8f89feb6c3d7af9bd179f7400e
Rémy Canal <remy.canal@live.fr>
2026-08-17 09:46:50 +0200

    fix(phase6): remove unnecessary regex escapes

--- src/features/pullRequests/views/createPullRequestView.ts
+++ src/features/pullRequests/views/createPullRequestView.ts
@@ -596,6 +596,6 @@ function escapeHtml(value: string): string {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&#039;");
 }
